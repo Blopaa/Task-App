d2b772d9a1b698e60fd44971359ebaa0840d1580
Pablo <pabloosabaterr@gmail.com>
2020-08-01 01:21:00 +0200

    created(build): for github pages

---
+++ docs/precache-manifest.ca5d735f03a56aebbf55f57608ad895e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb64b35d477aa410a7f68d15f33d90f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f42d61602c27c0a25637",
+    "url": "/static/css/main.96e9feab.chunk.css"
+  },
+  {
+    "revision": "845bbd02ef117c920227",
+    "url": "/static/js/2.99f7534b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99f7534b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42d61602c27c0a25637",
+    "url": "/static/js/main.bc762218.chunk.js"
+  },
+  {
+    "revision": "e7be01ffab14a9340e34",
+    "url": "/static/js/runtime-main.d40f2b6d.js"
+  }
+]);
