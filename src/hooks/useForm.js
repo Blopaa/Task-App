bbeb08e16f41ddcfce0e24e6ff79063479e4a016
Pablo <pabloosabaterr@gmail.com>
2020-08-01 01:12:06 +0200

    created(components, useForm): created the components and custom hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+export const useForm = (initialState = {}) => {
+   
+    const [values, setvalues] = useState(initialState)
+
+    const reset = () => {
+        setvalues(initialState);
+    }
+
+    const handleIputChange = ({target}) => {
+        setvalues({
+            ...values,
+            [target.name]: target.value
+        })
+    }
+
+    return [values, handleIputChange, reset];
+
+}
